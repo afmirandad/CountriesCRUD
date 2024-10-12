d6ba18eac74c9a6b2a66286f34c993f47179fff1
AndresMiranda <61157532+afmirandad@users.noreply.github.com>
2024-10-11 21:14:14 -0500

    Commit change

--- src/main/java/com/usbbog/countriesproject/controllers/CountriesController.java
+++ src/main/java/com/usbbog/countriesproject/controllers/CountriesController.java
@@ -1,5 +1,59 @@
 package com.usbbog.countriesproject.controllers;
 
-public class CountriesController {
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.usbbog.countriesproject.services.CountriesService;
+import com.usbbog.countriesproject.entities.CountriesEntity;
+
+@RestController
+@RequestMapping("/api/v1/countries")
+public class CountriesController {
+	
+	private final CountriesService countryService;
+	
+	@Autowired
+	public CountriesController(CountriesService countryService) {
+		this.countryService = countryService;
+	}
+	
+	//Method to get all countries
+	@GetMapping
+	public List<CountriesEntity> getCountries(){
+		return countryService.getAllCountries();
+	}
+	
+	@GetMapping("/{id}")
+	//Method to get one country by id
+	public Optional<CountriesEntity> getCountry(@PathVariable UUID id){
+		return countryService.getCountryById(id);
+	}
+	
+	@PostMapping
+	//Method to create one country
+	public String createCountry(){
+		return "POST COUNTRY";
+	}
+	
+	@PutMapping("/{id}")
+	//Method to update one country
+	public String updateCountry(){
+		return "PUT COUNTRY";
+	}
+	
+	@DeleteMapping("/{id}")
+	//Method to delete one country
+	public String deleteCountry(){
+		return "DELETE COUNTRY";
+	}
 }
--- src/main/java/com/usbbog/countriesproject/services/CountriesService.java
+++ src/main/java/com/usbbog/countriesproject/services/CountriesService.java
@@ -1,5 +1,71 @@
 package com.usbbog.countriesproject.services;
 
-public class CountriesService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+import com.usbbog.countriesproject.entities.CountriesEntity;
+
+@Service
+public class CountriesService {
+	private List<CountriesEntity> paises;
+	
+	public CountriesService() {
+		paises = new ArrayList<>();
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Qatar", "Doha", "Asia"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Ecuador", "Quito", "America del Sur"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Senegal", "Dakar", "Africa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Países Bajos", "Ámsterdam", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Inglaterra", "Londres", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Irán", "Teherán", "Asia"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Estados Unidos", "Washington D.C.", "America del Norte"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Gales", "Cardiff", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Argentina", "Buenos Aires", "America del Sur"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Arabia Saudita", "Riad", "Asia"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "México", "Ciudad de México", "America del Norte"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Polonia", "Varsovia", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Francia", "París", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Australia", "Camberra", "Oceanía"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Dinamarca", "Copenhague", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Túnez", "Túnez", "Africa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "España", "Madrid", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Costa Rica", "San José", "America Central"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Alemania", "Berlín", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Japón", "Tokio", "Asia"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Bélgica", "Bruselas", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Canadá", "Ottawa", "America del Norte"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Croacia", "Zagreb", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Brasil", "Brasilia", "America del Sur"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Portugal", "Lisboa", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Ghana", "Acra", "Africa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Uruguay", "Montevideo", "America del Sur"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Corea del Sur", "Seúl", "Asia"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Suiza", "Berna", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Camerún", "Yaundé", "Africa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Serbia", "Belgrado", "Europa"));
+		paises.add(new CountriesEntity(UUID.randomUUID(), "Marruecos", "Rabat", "Africa"));
+
+	}
+	
+	public List<CountriesEntity> getAllCountries(){
+		return paises;
+	}
+	
+	public Optional<CountriesEntity> getCountryById(UUID id){
+		return paises.stream().filter(country -> country.getId().equals(id)).findFirst();
+	}
+	
+	public List<> createCountry(){
+		
+	}
+	
+	public List<> updateCountry(){
+		
+	}
+	
+	public List<> deleteCountry(){
+		
+	}
 }
